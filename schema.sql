77c9fdb9a4a23bb1b8a01656e9852a0c4dc7e49c
Jonas Strassel <info@jonas-strassel.de>
2024-05-17 19:33:53 +0200

    :truck: Switch to renamed db

--- schema.sql
+++ schema.sql
@@ -4,6 +4,7 @@ CREATE TABLE urls (
     key CHAR(4) PRIMARY KEY,
     value TEXT NOT NULL,
     secret TEXT NOT NULL,
+    count INT DEFAULT 0,
     timestamp DATETIME DEFAULT current_timestamp
 );
 
